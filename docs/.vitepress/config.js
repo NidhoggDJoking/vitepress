ee60d25c9d8865f33a42819a9daa2a78872ff0e1
NidhoggDJoking <38490462+NidhoggDJoking@users.noreply.github.com>
2023-01-12 09:23:06 +0800

    2 files modified - algolia

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,21 +1,21 @@
 // const anchor = require("markdown-it-anchor")
 
-import nav from './configs/nav'
-import sidebar from './configs/sidebar'
-import socialLinks from './configs/socialLinks'
-import { BASEURL, NAME, YEAR } from './configs/global'
+import nav from './configs/nav';
+import sidebar from './configs/sidebar';
+import socialLinks from './configs/socialLinks';
+import { BASEURL, NAME, YEAR } from './configs/global';
 
 export default {
   // lang: 'en-US', // 默认为：en-US，<html lang="en-US">页面 HTML 中的标签
-  title: "Nidhogg·D·Joking", // 网站的标题。这将显示在导航栏中。也用作所有页面标题的后缀，除非titleTemplate已定义
+  title: 'Nidhogg·D·Joking', // 网站的标题。这将显示在导航栏中。也用作所有页面标题的后缀，除非titleTemplate已定义
   // titleTemplate: 'lala', // 设置false为禁用该功能。标题的后缀。
   // description: "我是站点的描述元标记", // HTML页面<meta>的描述
   lastUpdated: true, // git commit的最后更新时间
   head: [
     [
-      "link",
-      { rel: "shortcut icon", href: `favicon.ico`, type: "image/x-icon" }
-    ] // 在HTML页面<head>里插入元素
+      'link',
+      { rel: 'shortcut icon', href: `favicon.ico`, type: 'image/x-icon' },
+    ], // 在HTML页面<head>里插入元素
   ],
   ignoreDeadLinks: true, // 默认值false,忽略死链接，不会因为死链接而导致构建失败
   // appearance: false, // 主题是否暗色，默认true
@@ -23,7 +23,7 @@ export default {
   // outDir: "../public",   // 默认值为'./.vitepress/dist' 构建输出位置
   // 配置 Markdown 解析器
   markdown: {
-    theme: "material-palenight",
+    theme: 'material-palenight',
     lineNumbers: true, // 代码块启用行号
     // 目录改成悬浮才显示文字
     // anchor: {
@@ -37,29 +37,30 @@ export default {
     // }
   },
   themeConfig: {
-    logo: "/Bird.svg", //显示在导航栏中的徽标文件，位于站点标题之前。接受路径字符串或对象以为亮/暗模式设置不同的徽标。
-    siteTitle: "J-VitePress", // 替换导航中的默认站点标题，就是logo旁边的标题
-    outlineTitle: "On this page", // 可用于自定义右侧边栏的标题（在大纲链接的顶部）
-    lastUpdatedText: "最近更新时间：", // 默认：Last updated,在上次更新时间之前显示的前缀文本
+    logo: '/Bird.svg', //显示在导航栏中的徽标文件，位于站点标题之前。接受路径字符串或对象以为亮/暗模式设置不同的徽标。
+    siteTitle: 'J-VitePress', // 替换导航中的默认站点标题，就是logo旁边的标题
+    outlineTitle: 'On this page', // 可用于自定义右侧边栏的标题（在大纲链接的顶部）
+    lastUpdatedText: '最近更新时间：', // 默认：Last updated,在上次更新时间之前显示的前缀文本
     // 编辑链接
     editLink: {
       pattern: `https://gitee.com/${NAME}/${BASEURL}/edit/master/docs/:path`,
-      text: "在 Gitee/Github 上编辑此页面"
+      text: '在 Gitee/Github 上编辑此页面',
     },
     // 可用于自定义出现在上一个和下一个链接上方的文本
     docFooter: {
-      prev: "上一页",
-      next: "下一页"
+      prev: '上一页',
+      next: '下一页',
     },
     // 页脚 仅当页面不包含侧边栏时才会显示页脚。
     footer: {
       message: '在 MIT 许可下发布',
-      copyright: `Copyright (c) ${YEAR}-present ${NAME}`
+      copyright: `Copyright (c) ${YEAR}-present ${NAME}`,
     },
     algolia: {
       appId: 'SO2MMF1HZC',
-      apiKey:'daa25d6a1618b680a3074f90e47b9236',
-      indexName: 'gitee_datasets'
+      apiKey: '62b2fa4b766a59f92958ba894d57a6d9',
+      indexName: 'datasets',
+      chunkSize: 5000
     },
     // 碳广告 显示碳广告的选项。
     // carbonAds: {
@@ -68,6 +69,6 @@ export default {
     // },
     nav,
     sidebar,
-    socialLinks
-  }
-}
+    socialLinks,
+  },
+};
