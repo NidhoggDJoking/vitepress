0bb97c94d22ef732ba9ee8dd515180df1ad694b5
NidhoggDJoking <250348426@qq.com>
2023-01-21 22:37:23 +0800

    feat:happy new yera

--- docs/.vitepress/configs/nav.js
+++ docs/.vitepress/configs/nav.js
@@ -1,15 +1,5 @@
 // 导航菜单项的配置
 export default [
-    // { text: "指南", link: "/guide/什么是 VitePress？" },
-    // {
-    //     text: "配置",
-    //     items: [
-    //         {
-    //             text: "常用配置",
-    //             items: [{ text: "主要配置", link: "/config/介绍" }]
-    //         }
-    //     ]
-    // }
     { text: 'Note', link: '/note/note', activeMatch: '/note/' },
     { text: 'Guide', link: '/guide/what-is-vitepress', activeMatch: '/guide/' },
     { text: 'Configs', link: '/config/introduction', activeMatch: '/config/' },
