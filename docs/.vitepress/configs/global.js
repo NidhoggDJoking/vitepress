0adc75b68099e37bfa8fb47eb5b49812c93701aa
NidhoggDJoking <250348426@qq.com>
2023-01-21 23:24:59 +0800

    chore:兼容Gitee pages

--- docs/.vitepress/configs/global.js
+++ docs/.vitepress/configs/global.js
@@ -1,4 +1,4 @@
 export const YEAR = new Date().getFullYear()
-export const BASEURL = 'VitePress'
+export const BASEURL = 'vitepress'
 // export const BASEURL = window.location.host === '' ? 'vitepress' : 'VitePress'
 export const NAME = 'NidhoggDJoking'
