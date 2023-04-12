06ea7089952385bfc2a4a4c8d9085e9dfc64b1a2
NidhoggDJoking <NidhoggDJoking@users.noreply.github.com>
2023-04-12 12:00:11 +0000

    deploy: c94f5b40f0210dbe6a27c4839fd509837c5fea6a

---
+++ assets/note_element_table.md.a964ab6a.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,o as a,c as s,a as l}from"./app.62d0d417.js";const u=JSON.parse('{"title":"el-table","description":"","frontmatter":{},"headers":[],"relativePath":"note/element/table.md","lastUpdated":1680765431000}'),t={name:"note/element/table.md"},n=l("",4),o=[n];function c(p,r,d,i,_,D){return a(),s("div",null,o)}const F=e(t,[["render",c]]);export{u as __pageData,F as default};
