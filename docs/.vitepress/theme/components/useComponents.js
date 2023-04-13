c5b09be77c47383d141744b1ce2792298a1d0d1d
Nidhogg·D·Joking <250348426@qq.com>
2023-04-13 03:16:40 +0000

    add useDraggable component

--- docs/.vitepress/theme/components/useComponents.js
+++ docs/.vitepress/theme/components/useComponents.js
@@ -1,7 +1,9 @@
 // Don't remove this file, because it registers the demo components.
 // import music from './music.vue'
 import collapse from './collapse.vue'
+import { useDraggable } from '@vueuse/core'
 
 export function useComponents(app) {
   app.component('collapse', collapse)
+  app.component('useDraggable', useDraggable)
 }
