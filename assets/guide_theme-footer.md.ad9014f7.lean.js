e772c96c790b35f22c8d97851c355669020d981b
NidhoggDJoking <NidhoggDJoking@users.noreply.github.com>
2023-05-26 11:17:23 +0000

    deploy: 869ece9a3aa8b7723aae9d7ed0e60967e2fc1489

---
+++ assets/guide_theme-footer.md.ad9014f7.lean.js
@@ -0,0 +1,1 @@
+import{_ as s,o as n,c as a,a as e}from"./app.4854f1f3.js";const b=JSON.parse('{"title":"Footer","description":"","frontmatter":{},"headers":[],"relativePath":"guide/theme-footer.md","lastUpdated":1673445587000}'),l={name:"guide/theme-footer.md"},p=e("",7),o=[p];function t(r,c,i,D,y,F){return n(),a("div",null,o)}const u=s(l,[["render",t]]);export{b as __pageData,u as default};
